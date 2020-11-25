5a870419b083a6eea44aab3feffa2584eb02caf6
Matías Prego <yo@matiasprego.com>
2020-11-25 19:32:31 -0300

    erase links

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,92 +1,95 @@
-// Definición de variables
-var tipoMovimiento = ["Anual", "Mensual", "Semanal"];
-var tipoOperacion = ["Gasto", "Ingreso"];
+allFunctions();
+function allFunctions() {
+    // Definición de variables
+  var tipoMovimiento = ["Anual", "Mensual", "Semanal"];
+  var tipoOperacion = ["Gasto", "Ingreso"];
 
-var monedasBasicas = ["Pesos", "Dólares"];
-var monedasSecundarias = ["Euros", "Reales"];
-var todasMonedas = monedasBasicas.concat(monedasSecundarias);
+  var monedasBasicas = ["Pesos", "Dólares"];
+  var monedasSecundarias = ["Euros", "Reales"];
+  var todasMonedas = monedasBasicas.concat(monedasSecundarias);
 
-// Ingreso de datos
-class nuevoDato {
-    constructor(entrada) {
-      this.tipo = getTipo; //define si es ingreso o egreso
-      this.monto = getMonto;
-      this.moneda = getMoneda; //define si es pesos o dolares (inicialmente)
-      if (moneda = 'USD') {
-          this.cotizacion = getCotizacion;
+  // Ingreso de datos
+  class nuevoDato {
+      constructor(entrada) {
+        this.tipo = getTipo; //define si es ingreso o egreso
+        this.monto = getMonto;
+        this.moneda = getMoneda; //define si es pesos o dolares (inicialmente)
+        if (moneda = 'USD') {
+            this.cotizacion = getCotizacion;
+        }
+        this.repeticion = getRepeticion;
       }
-      this.repeticion = getRepeticion;
-    }
-}
+  }
 
-// Manejo de monedas
-function nuevaMoneda(nombre, cotizacion) {
-    this.nombre = nombre;
-    this.cotizacion = cotizacion;
-}
+  // Manejo de monedas
+  function nuevaMoneda(nombre, cotizacion) {
+      this.nombre = nombre;
+      this.cotizacion = cotizacion;
+  }
 
-function calcularEnMonedaExtranjera (monto, moneda) {
-    this.monto = monto;
-    var cambioMoneda = this.moneda[1];
-    var resultado = monto.cambioMoneda;
-}
+  function calcularEnMonedaExtranjera (monto, moneda) {
+      this.monto = monto;
+      var cambioMoneda = this.moneda[1];
+      var resultado = monto.cambioMoneda;
+  }
 
-// Manejo del form
+  // Manejo del form
 
-function formValues() {
-  $( "#btn-form" ).click(function() {
-    var camposForm = $( ":input" ).serializeArray();
-    console.log(camposForm);
-    //guardar datos
-    localStorage.setItem("dato", JSON.stringify(camposForm));
-    document.getElementById("overlay").style.display = "none";
-  });
-}
+  function formValues() {
+    $( "#btn-form" ).click(function() {
+      var camposForm = $( ":input" ).serializeArray();
+      console.log(camposForm);
+      //guardar datos
+      localStorage.setItem("dato", JSON.stringify(camposForm));
+      document.getElementById("overlay").style.display = "none";
+    });
+  }
 
-//$( ":checkbox, :radio" ).click( formValues );
-//$( "select" ).change( formValues );
-formValues();
+  //$( ":checkbox, :radio" ).click( formValues );
+  //$( "select" ).change( formValues );
+  formValues();
 
-// Tomar datos
+  // Tomar datos
 
-function crearTabla() {
-  var datosStorage = localStorage.getItem("dato");
-  var listado = JSON.parse(datosStorage);
-  console.log(listado);
-}
-crearTabla();
+  function crearTabla() {
+    var datosStorage = localStorage.getItem("dato");
+    var listado = JSON.parse(datosStorage);
+    console.log(listado);
+  }
+  crearTabla();
 
 
-// Botones principales
+  // Botones principales
 
-$( ".AddBtn" ).click(function(e) {
+  $( ".AddBtn" ).click(function(e) {
+      document.getElementById("overlay").style.display = "block";
+    });
+
+  $( ".CloseBtn" ).click(function(e) {
+      document.getElementById("overlay").style.display = "none";
+    });
+
+  $( ".AddEntrada" ).click(function(e) {
     document.getElementById("overlay").style.display = "block";
+    document.getElementById("entrada").checked = true;
+  });
+  $( ".AddSalida" ).click(function(e) {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("salida").checked = true;
   });
 
-$( ".CloseBtn" ).click(function(e) {
-    document.getElementById("overlay").style.display = "none";
+
+
+  $( ".simple-link" ).click(function(e) {
+    console.log("entre");
+    e.preventDefault();
+    e.stopPropagation();
+    var href = $(this).attr('href');
+    $.get(href, function(data, status){
+      $('#content-area').html(data);
+    });
   });
-
-$( ".AddEntrada" ).click(function(e) {
-  document.getElementById("overlay").style.display = "block";
-  document.getElementById("entrada").checked = true;
-});
-$( ".AddSalida" ).click(function(e) {
-  document.getElementById("overlay").style.display = "block";
-  document.getElementById("salida").checked = true;
-});
-
-
-
-$( ".simple-link" ).click(function(e) {
-  console.log("entre");
-  e.preventDefault();
-	e.stopPropagation();
-  var href = $(this).attr('href');
-  $.get(href, function(data, status){
-    $('#content-area').html(data);
-  });
-});
+}
 
 $( document ).ready(function() {
   $.get("principal.html", function(data, status){
@@ -98,6 +101,7 @@ $( document ).ready(function() {
       var href = $(this).attr('href');
       $.get(href, function(data, status){
         $('#content-area').html(data);
+        allFunctions();
       });
     });
   });
