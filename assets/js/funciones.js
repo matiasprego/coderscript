89bb9e24309dfe46df2aa2dea3b5bf4bbb8141d6
Matías Prego <yo@matiasprego.com>
2020-12-20 20:17:30 -0300

    calculos totales

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -2,14 +2,15 @@ allFunctions();
 function allFunctions() {
 
   // Chequeo si tengo guardado en localstore los datosStorage
-  if(localStorage.getItem('dato')){
+  if (localStorage.getItem('dato')) {
     var listado = JSON.parse(localStorage.getItem('dato'));
     crearTabla();
+    calculo();
   } else {
     var listado = [];
   }
 
-    // Definición de variables
+  // Definición de variables
   var tipoMovimiento = ["Anual", "Mensual", "Semanal"];
   var tipoOperacion = ["Gasto", "Ingreso"];
 
@@ -19,36 +20,36 @@ function allFunctions() {
 
   // Ingreso de datos
   class nuevoDato {
-      constructor(entrada) {
-        this.tipo = getTipo; //define si es ingreso o egreso
-        this.monto = getMonto;
-        this.moneda = getMoneda; //define si es pesos o dolares (inicialmente)
-        if (moneda = 'USD') {
-            this.cotizacion = getCotizacion;
-        }
-        this.repeticion = getRepeticion;
+    constructor(entrada) {
+      this.tipo = getTipo; //define si es ingreso o egreso
+      this.monto = getMonto;
+      this.moneda = getMoneda; //define si es pesos o dolares (inicialmente)
+      if (moneda = 'USD') {
+        this.cotizacion = getCotizacion;
       }
+      this.repeticion = getRepeticion;
+    }
   }
 
   // Manejo de monedas
   function nuevaMoneda(nombre, cotizacion) {
-      this.nombre = nombre;
-      this.cotizacion = cotizacion;
+    this.nombre = nombre;
+    this.cotizacion = cotizacion;
   }
 
-  function calcularEnMonedaExtranjera (monto, moneda) {
-      this.monto = monto;
-      var cambioMoneda = this.moneda[1];
-      var resultado = monto.cambioMoneda;
+  function calcularEnMonedaExtranjera(monto, moneda) {
+    this.monto = monto;
+    var cambioMoneda = this.moneda[1];
+    var resultado = monto.cambioMoneda;
   }
 
   // Manejo del form
 
   function formValues() {
-    $( "#btn-form" ).click(function() {
+    $("#btn-form").click(function () {
       var camposForm = {};
-      $.each($('form#form-entry').serializeArray(), function() {
-          camposForm[this.name] = this.value;
+      $.each($('form#form-entry').serializeArray(), function () {
+        camposForm[this.name] = this.value;
       });
       // LO AGREGO AL ARRAY DE OBJETOS DEL LISTADO
       console.log('[camposForm]', camposForm);
@@ -58,7 +59,6 @@ function allFunctions() {
       document.getElementById("overlay").style.display = "none";
 
       // Actualizo tabla
-      $('tbody').html("");
       crearTabla();
 
     });
@@ -72,78 +72,128 @@ function allFunctions() {
 
   // Botones principales
 
-  $( ".AddBtn" ).click(function(e) {
-      document.getElementById("overlay").style.display = "block";
-    });
+  $(".AddBtn").click(function (e) {
+    document.getElementById("overlay").style.display = "block";
+  });
 
-  $( ".CloseBtn" ).click(function(e) {
-      document.getElementById("overlay").style.display = "none";
-    });
+  $(".CloseBtn").click(function (e) {
+    document.getElementById("overlay").style.display = "none";
+  });
 
-  $( ".AddEntrada" ).click(function(e) {
+  $(".AddEntrada").click(function (e) {
     document.getElementById("overlay").style.display = "block";
     document.getElementById("entrada").checked = true;
   });
-  $( ".AddSalida" ).click(function(e) {
+  $(".AddSalida").click(function (e) {
     document.getElementById("overlay").style.display = "block";
     document.getElementById("salida").checked = true;
   });
 
 
 
-  $( ".simple-link" ).click(function(e) {
+  $(".simple-link").click(function (e) {
     e.preventDefault();
     e.stopPropagation();
     var href = $(this).attr('href');
-    $.get(href, function(data, status){
+    $.get(href, function (data, status) {
       $('#content-area').html(data);
     });
   });
 }
 
+function calculo() {
+  let totalResta = 0;
+  let totalSuma = 0;
+  if (localStorage.getItem('totalResta')) {
+    totalResta = localStorage.getItem('totalResta');
+  }
+  if (localStorage.getItem('totalSuma')) {
+    totalSuma = localStorage.getItem('totalSuma');
+  }
+  var gastosMensuales = totalResta / 12;
+  var ingresosMensuales = totalSuma / 12;
+  var total = ingresosMensuales - gastosMensuales;
+  $('montoGastos').append(gastosMensuales);
+  $('montoIngresos').append(ingresosMensuales);
+  $('montoBalance').append(total);
+
+}
+
 // Tomar datos
 function crearTabla() {
+  $('tbody').html("");
+
   var datosStorage = localStorage.getItem("dato");
   var listado = JSON.parse(datosStorage);
-
-  if (listado){
+  let totalResta = 0;
+  let totalSuma = 0;
+  if (listado) {
+    if (localStorage.getItem('totalResta')) {
+      totalResta = localStorage.getItem('totalResta');
+    }
+    if (localStorage.getItem('totalSuma')) {
+      totalSuma = localStorage.getItem('totalSuma');
+    }
     //console.log(listado);
     var htmlTable = '';
-    $.each( listado, function( key, row ) {
+    $.each(listado, function (key, row) {
       htmlTable += '<tr>';
       htmlTable += '<td>#</td>';
-      htmlTable += '<td>'+ row.nombre +'</td>';
-      htmlTable += '<td>'+ row.tipoDato +'</td>';
-      htmlTable += '<td>'+ row.repeticion +'</td>';
-      htmlTable += '<td>'+ row.moneda +'</td>';
-      htmlTable += '<td>'+ row.amount +'</td>';
-      if(row.repeticion == "Semanal") {
-        htmlTable += '<td>'+ (row.amount*52) +'</td>';
+      htmlTable += '<td>' + row.nombre + '</td>';
+      htmlTable += '<td>' + row.tipoDato + '</td>';
+      htmlTable += '<td>' + row.repeticion + '</td>';
+      htmlTable += '<td>' + row.moneda + '</td>';
+      htmlTable += '<td>' + row.amount + '</td>';
+      if (row.repeticion == "Semanal") {
+        htmlTable += '<td>' + (row.amount * 52) + '</td>';
+        if (row.tipoDato == "entrada") {
+          totalSuma += (row.amount * 52);
+        } else if (row.tipoDato == "salida") {
+          totalResta += (row.amount * 52);
+        }
       }
-      else if(row.repeticion == "Mensual") {
-        htmlTable += '<td>'+ (row.amount*12) +'</td>';
+      else if (row.repeticion == "Mensual") {
+        htmlTable += '<td>' + (row.amount * 12) + '</td>';
+        if (row.tipoDato == "entrada") {
+          totalSuma += (row.amount * 12);
+        } else if (row.tipoDato == "salida") {
+          totalResta += (row.amount * 12);
+        }
       }
-      else if(row.repeticion == "Diario") {
-        htmlTable += '<td>'+ (row.amount*365) +'</td>';
+      else if (row.repeticion == "Diario") {
+        htmlTable += '<td>' + (row.amount * 365) + '</td>';
+        if (row.tipoDato == "entrada") {
+          totalSuma += (row.amount * 365);
+        } else if (row.tipoDato == "salida") {
+          totalResta += (row.amount * 365);
+        }
       }
       else {
-        htmlTable += '<td>'+ row.amount +'</td>';
+        htmlTable += '<td>' + row.amount + '</td>';
+        if (row.tipoDato == "entrada") {
+          totalSuma += row.amount;
+        } else if (row.tipoDato == "salida") {
+          totalResta += row.amount;
+        }
       }
       htmlTable += '</tr>';
+
     });
-    
+    localStorage.setItem('totalResta', totalResta);
+    localStorage.setItem('totalSuma', totalSuma);
+
     $('tbody').append(htmlTable);
   }
 }
 
-$( document ).ready(function() {
-  $.get("principal.html", function(data, status){
+$(document).ready(function () {
+  $.get("principal.html", function (data, status) {
     $('#content-area').html(data);
-    $( ".simple-link" ).click(function(e) {
+    $(".simple-link").click(function (e) {
       e.preventDefault();
       e.stopPropagation();
       var href = $(this).attr('href');
-      $.get(href, function(data, status){
+      $.get(href, function (data, status) {
         $('#content-area').html(data);
         allFunctions();
         //crearTabla();
@@ -152,3 +202,4 @@ $( document ).ready(function() {
     });
   });
 });
+
